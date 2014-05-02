1fd4f765d9c8673aa2389313525fc2cb7ffcbbc6
Weston Odom <weston.odom@gmail.com>
2014-05-01 21:44:11 -0500

    started writing tests

---
+++ tests.py
@@ -0,0 +1,22 @@
+import bcrypt
+import dbmonitor
+import pymysql
+import unittest
+
+
+class TestDBMonitor(unittest.TestCase):
+
+	def test_db_connect(self):
+		pass
+
+	def test_db_disconnect(self):
+		pass
+
+	def test_get_db2_status(self):
+		pass
+
+	def test_get_db1_old_records(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
